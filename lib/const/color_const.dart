9421b7460b4718347734fe5d52b5797550e98d57
Tanbir Hossain <mdtanbirhossain1433@gmail.com>
2024-05-08 21:15:57 +0600

    v 1.4

--- lib/const/color_const.dart
+++ lib/const/color_const.dart
@@ -4,10 +4,10 @@
 import 'package:flutter/material.dart';
 
 class MyColor{
-  static const appColor = Color(0xff13875f);
+  static const appColor = Color(0xff118C6F);
   static const appWhiteColor = Color(0xffffffff);
   static const appScaffoldColor = Color(0xffEDF2F4);
   static const appBlackColor = Color(0xff000000);
-  static const buttonColor = Color(0xff13875f);
+  static const buttonColor = Color(0xff118C6F);
   static const color = Color(0xff46B891);
 }
